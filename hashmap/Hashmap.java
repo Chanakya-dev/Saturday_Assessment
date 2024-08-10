22b7fd92b389df38e5b5a606042a3909b13307a7
siva-245711 <sivanagendra809@gmail.com>
2024-08-10 11:30:39 +0530

    assinment

---
+++ hashmap/Hashmap.java
@@ -0,0 +1,19 @@
+import static java.awt.AWTEventMulticaster.add;
+import java.util.HashMap;
+import java.util.Set;
+
+public class Hashmap {
+    public static void main(String[] args) {}
+
+    public static void hashmap() {
+        HashMap<Person, String> hm = new HashMap<Person, String>();
+        hm.add(1, "a");
+        hm.add(2, "b");
+         
+        for (Person hm : add) {
+            System.out.println(hm);
+            
+        }
+    
+}}
+   
